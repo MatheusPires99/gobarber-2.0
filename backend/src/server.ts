8e5da84e931283ae482efc4182b81d259cfd1bcb
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-16 17:06:17 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log("🚀 Server started on port 3333!");
+});
