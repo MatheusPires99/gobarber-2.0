fbd72729e6f53aa5385b94ea9ac5690c542dfcd5
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-21 18:06:10 -0300

    Using unform on input component and signUp form

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { useEffect, useRef, InputHTMLAttributes } from "react";
 import { IconBaseProps } from "react-icons";
+import { useField } from "@unform/core";
 
 import { Container } from "./styles";
 
@@ -9,11 +10,22 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <input {...rest} />
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
     </Container>
   );
 };
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
 
 import logo from "../../assets/logo.svg";
 
@@ -9,6 +10,10 @@ import Button from "../../components/Button";
 import { Container, Content, Background } from "./styles";
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
@@ -16,7 +21,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
@@ -29,7 +34,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="criar">
           <FiArrowLeft />
