d3a1382b055a117ba381815967c2b243ae3300c0
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-21 14:33:41 -0300

    Global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import GlobalStyle from "./styles/global";
+
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
 export default App;
