60adddf841668110b44f103f3490ab0f0c9b633b
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-22 14:57:34 -0300

    Create private routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+import Route from "./Route";
 
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
