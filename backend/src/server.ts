bda237d9679dc0ace1c3812c5b86de96449f2793
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-21 23:41:17 -0300

    Cors config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,8 +26,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
-  console.error(err);
-
   return response.status(500).json({
     status: "error",
     message: "Internal server error",
