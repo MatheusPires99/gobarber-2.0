22519ff32c2f830a43c3ef97b17ce24f1b378d7c
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-22 10:28:58 -0300

    Logout from the app

--- frontend/src/hooks/AuthContext.tsx
+++ frontend/src/hooks/AuthContext.tsx
@@ -15,6 +15,7 @@ interface AuthState {
 interface AuthContextData {
   user: object;
   signIn(credencials: SignInCredencials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -48,8 +49,15 @@ export const AuthProvider: React.FC = ({ children }) => {
     });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem("@GoBarber:token");
+    localStorage.removeItem("@GoBarber:user");
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
 
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../hooks/AuthContext";
 
 import getValidationErrors from "../../utils/getValidationErrors";
 
