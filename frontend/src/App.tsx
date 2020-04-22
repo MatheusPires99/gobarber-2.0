c4a235e298e666339c6873839938d2ba13397e91
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-21 23:39:00 -0300

    Input validation on signIn page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <SignUp />
+      <SignIn />
     </>
   );
 };
--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import * as Yup from "yup";
+
+import getValidationErrors from "../../utils/getValidationErrors";
 
 import logo from "../../assets/logo.svg";
 
@@ -9,12 +14,35 @@ import Button from "../../components/Button";
 import { Container, Content, Background } from "./styles";
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email("Digite um e-mail válido")
+          .required("E-mail obrigatório"),
+        password: Yup.string().required("Senha obrigatória"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
@@ -28,7 +56,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="criar">
           <FiLogIn />
