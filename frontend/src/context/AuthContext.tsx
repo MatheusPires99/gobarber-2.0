1529a21792914bbb10af0b0eb7df4817c3909001
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-22 09:14:26 -0300

    Keeping user on the storage

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from "react";
+import React, { createContext, useCallback, useState } from "react";
 
 import api from "../services/api";
 
@@ -7,8 +7,13 @@ interface SignInCredencials {
   password: string;
 }
 
+interface AuthState {
+  user: object;
+  token: string;
+}
+
 interface AuthContextData {
-  name: string;
+  user: object;
   signIn(credencials: SignInCredencials): Promise<void>;
 }
 
@@ -17,17 +22,36 @@ export const AuthContext = createContext<AuthContextData>(
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem("@GoBarber:token");
+    const user = localStorage.getItem("@GoBarber:user");
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post("sessions", {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { user, token } = response.data;
+
+    localStorage.setItem("@GoBarber:token", token);
+    localStorage.setItem("@GoBarber:user", JSON.stringify(user));
+
+    setData({
+      user,
+      token,
+    });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: "Matheus", signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -23,7 +23,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { signIn, user } = useContext(AuthContext);
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
