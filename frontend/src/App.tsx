6dc015be33b2b03a475276b5206a99b9fb70f3c7
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-22 08:43:33 -0300

    Context API first config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,19 @@ import React from "react";
 import GlobalStyle from "./styles/global";
 
 import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+// import SignUp from "./pages/SignUp";
+
+import AuthContext from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
     <>
+      <AuthContext.Provider value={{ name: "Matheus" }}>
+        <GlobalStyle />
+        <SignIn />
+      </AuthContext.Provider>
+
       <GlobalStyle />
-      <SignIn />
     </>
   );
 };
--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useContext } from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
+
+import AuthContext from "../../context/AuthContext";
 
 import getValidationErrors from "../../utils/getValidationErrors";
 
@@ -15,6 +17,8 @@ import { Container, Content, Background } from "./styles";
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
