ea4a0d9f4dee7347f4914a14326007b9233015bd
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-21 23:40:51 -0300

    Cors config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import routes from "./routes";
 import uploadConfig from "./config/upload";
@@ -12,6 +13,7 @@ import "./database";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
