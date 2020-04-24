9e28932b7b1aecb25cc43437e026b99b84090417
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-24 11:18:27 -0300

    Statusbar config

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StatusBar } from "react-native";
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312338" />
+      <View style={{ flex: 1, backgroundColor: "#312338" }} />
+    </>
+  );
 };
 
 export default App;
