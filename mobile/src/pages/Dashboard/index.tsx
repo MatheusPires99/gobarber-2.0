a176610a1ae684a4e1bd688022f65dd937158d05
Matheus Pires <matheushenriquepires99@gmail.com>
2020-04-27 09:47:11 -0300

    SignOut button

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Button } from "react-native";
+
+import { useAuth } from "../../hooks/auth";
 
 const Dashboard: React.FC = () => {
-  return <View />;
+  const { signOut } = useAuth();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Button title="Sair" onPress={signOut} />
+    </View>
+  );
 };
 
 export default Dashboard;
