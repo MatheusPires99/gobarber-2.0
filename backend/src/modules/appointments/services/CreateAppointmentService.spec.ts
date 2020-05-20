8764dc711584ac2c9fe48b6b6529aea205013fd3
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-20 20:30:35 -0300

    Appointments rules

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -6,16 +6,20 @@ import CreateAppointmentService from "./CreateAppointmentService";
 let fakeAppointmentRepository: FakeAppointmentRepository;
 let createAppointment: CreateAppointmentService;
 
-describe("CreateAPpointment", () => {
+describe("CreateAppointment", () => {
   beforeEach(() => {
     fakeAppointmentRepository = new FakeAppointmentRepository();
     createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
   });
 
   it("should be able to create a new appointment", async () => {
+    jest.spyOn(Date, "now").mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
     const appointment = await createAppointment.execute({
-      date: new Date(),
-      user_id: "123123",
+      date: new Date(2020, 4, 10, 13),
+      user_id: "123456",
       provider_id: "123123",
     });
 
@@ -24,19 +28,69 @@ describe("CreateAPpointment", () => {
   });
 
   it("should not be able to create two appointments on the same date", async () => {
-    const appointmentDate = new Date(2020, 4, 10, 11);
+    const appointmentDate = new Date(2020, 4, 10, 13);
 
     await createAppointment.execute({
       date: appointmentDate,
-      user_id: "123123",
+      user_id: "123456",
       provider_id: "123123",
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: "123456",
+        provider_id: "123123",
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create an appointment on a past date", async () => {
+    jest.spyOn(Date, "now").mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 11),
+        user_id: "123456",
+        provider_id: "123123",
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create an appointment with same user as provider", async () => {
+    jest.spyOn(Date, "now").mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 13),
         user_id: "123123",
         provider_id: "123123",
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create an appointment before 8am and after 5pm", async () => {
+    jest.spyOn(Date, "now").mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 7),
+        user_id: "123123",
+        provider_id: "123456",
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 18),
+        user_id: "123123",
+        provider_id: "123456",
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from "date-fns";
+import { startOfHour, isBefore, getHours } from "date-fns";
 import { injectable, inject } from "tsyringe";
 
 import AppError from "@shared/errors/AppError";
@@ -26,9 +26,25 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        "You can only create appointments between 8am and 5pm",
+      );
+    }
+
     const findAppoitmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
+
+    console.log(findAppoitmentInSameDate?.date);
 
     if (findAppoitmentInSameDate) {
       throw new AppError("This appointment is already booked");
