169942bf6f0594c888a85c3ba751647623060262
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-23 11:06:25 -0300

    Refctoring test for notifications

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,15 +1,22 @@
 import AppError from "@shared/errors/AppError";
 
+import FakeNotificationsRepository from "@modules/notifications//repositories/fakes/FakeNotificationsRepository";
 import FakeAppointmentRepository from "../repositories/Fakes/FakeAppointmentsRepository";
 import CreateAppointmentService from "./CreateAppointmentService";
 
 let fakeAppointmentRepository: FakeAppointmentRepository;
+let fakeNotificationsRepository: FakeNotificationsRepository;
 let createAppointment: CreateAppointmentService;
 
 describe("CreateAppointment", () => {
   beforeEach(() => {
     fakeAppointmentRepository = new FakeAppointmentRepository();
-    createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+    fakeNotificationsRepository = new FakeNotificationsRepository();
+
+    createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+      fakeNotificationsRepository,
+    );
   });
 
   it("should be able to create a new appointment", async () => {
