7ac2e31ad409c63a6c987d011c9784ac790df36e
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-23 10:56:13 -0300

    Sending notifications

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,9 @@
-import { startOfHour, isBefore, getHours } from "date-fns";
+import { startOfHour, isBefore, getHours, format } from "date-fns";
 import { injectable, inject } from "tsyringe";
 
 import AppError from "@shared/errors/AppError";
 
+import INotificationsRepository from "@modules/notifications/repositories/INotificationsRepository";
 import Appointment from "../infra/typeorm/entities/Appointment";
 import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 
@@ -17,6 +18,9 @@ class CreateAppointmentService {
   constructor(
     @inject("AppointmentsRepository")
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject("NotificationsRepository")
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({
@@ -52,6 +56,13 @@ class CreateAppointmentService {
       provider_id,
       user_id,
       date: appointmentDate,
+    });
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${dateFormatted}`,
     });
 
     return appointment;
--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -12,6 +12,9 @@ import UsersRepository from "@modules/users/infra/typeorm/repositories/UsersRepo
 import IUsersTokensRepository from "@modules/users/repositories/IUserTokensRepository";
 import UsersTokensRepository from "@modules/users/infra/typeorm/repositories/UserTokensRepository";
 
+import INotificationsRepository from "@modules/notifications/repositories/INotificationsRepository";
+import NotificationsRepository from "@modules/notifications/infra/typeorm/repositories/NotificationsRepository";
+
 container.registerSingleton<IAppointmentsRepository>(
   "AppointmentsRepository",
   AppointmentsRepository,
@@ -25,4 +28,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUsersTokensRepository>(
   "UserTokensRepository",
   UsersTokensRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+  "NotificationsRepository",
+  NotificationsRepository,
 );
