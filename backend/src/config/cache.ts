8be5bd1a11538eeda327dc9087def912cc26c508
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-25 21:59:40 -0300

    Express rate limiter

--- backend/src/config/cache.ts
+++ backend/src/config/cache.ts
@@ -13,9 +13,9 @@ export default {
 
   config: {
     redis: {
-      host: "localhost",
-      port: 6379,
-      password: undefined,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS || undefined,
     },
   },
 } as ICacheConfig;
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -9,12 +9,14 @@ import cors from "cors";
 import uploadConfig from "@config/upload";
 import AppError from "@shared/errors/AppError";
 import routes from "./routes";
+import rateLimiter from "./middlewares/rateLimiter";
 
 import "@shared/infra/typeorm";
 import "@shared/container";
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.uploadsFolder));
