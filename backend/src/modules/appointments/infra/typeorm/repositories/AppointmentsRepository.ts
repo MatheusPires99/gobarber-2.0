a5911027baf01350b1cb45c767148822aebfb49a
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-25 23:33:28 -0300

    Users relations on appointments

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -62,6 +62,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ["user"],
     });
 
     return appointments;
