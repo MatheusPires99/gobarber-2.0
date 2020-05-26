7172295cdc4fefc4a2bc2696faf8e5162f58a619
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-25 23:27:50 -0300

    Unavailable days in month

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from "tsyringe";
-import { getDaysInMonth, getDate } from "date-fns";
+import { getDaysInMonth, getDate, isAfter } from "date-fns";
 
 import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 
@@ -42,13 +42,16 @@ class ListProvidersMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
