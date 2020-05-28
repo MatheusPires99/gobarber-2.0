1150bc199f690d370c564b7717bae197c0831785
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-27 21:28:48 -0300

    Creating reset password page

---
+++ frontend/src/pages/ResetPassword/index.tsx
@@ -0,0 +1,107 @@
+import React, { useRef, useState, useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import * as Yup from "yup";
+
+import { useToast } from "../../hooks/toast";
+
+import getValidationErrors from "../../utils/getValidationErrors";
+
+import logo from "../../assets/logo.svg";
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+import { Container, Content, AnimationContainer, Background } from "./styles";
+
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          password: Yup.string().required("Nova senha obrigatória"),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref("password"), null],
+            "Confirmação incorreta",
+          ),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        setLoading(true);
+
+        history.push("/");
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: "error",
+          title: "Erro ao resetar senha",
+          description: "Ocorreu um erro ao resetar sua senha, tente novamente.",
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logo} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Resetar senha</h1>
+
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Nova senha"
+            />
+            <Input
+              name="password_confirmation"
+              icon={FiLock}
+              type="password"
+              placeholder="Confirmação da senha"
+            />
+
+            <Button loading={loading} type="submit">
+              Alterar senha
+            </Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,6 +6,7 @@ import Route from "./Route";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
 import Dashboard from "../pages/Dashboard";
 
 const Routes: React.FC = () => {
@@ -14,6 +15,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
