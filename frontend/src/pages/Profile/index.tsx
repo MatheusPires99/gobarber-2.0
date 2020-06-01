a93f5f4fe6f5dcad989b8525d97bc5d0c09ba537
Matheus Pires <matheushenriquepires99@gmail.com>
2020-06-01 18:53:08 -0300

    Updating user data

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -20,7 +20,9 @@ import { Container, Content, AvatarInput } from "./styles";
 interface ProfileFormData {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
@@ -43,7 +45,19 @@ const Profile: React.FC = () => {
           email: Yup.string()
             .email("Digite um e-mail válido")
             .required("E-mail obrigatório"),
-          password: Yup.string().min(6, "No mínimo 6 digitos"),
+          old_password: Yup.string(),
+          password: Yup.string().when("old_password", {
+            is: val => !!val.length,
+            then: Yup.string().required("Campo obrigatório"),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when("old_password", {
+              is: val => !!val.length,
+              then: Yup.string().required("Campo obrigatório"),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref("password"), null], "Confirmação incorreta"),
         });
 
         await schema.validate(data, {
@@ -52,12 +66,37 @@ const Profile: React.FC = () => {
 
         setLoading(true);
 
-        await api.post("users", data);
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? {
+                old_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put("/profile", formData);
+
+        updateUser(response.data);
+
+        history.push("/dashboard");
 
         addToast({
           type: "success",
-          title: "Cadastro realizado",
-          description: "Você já pode fazer seu logon no GoBarber",
+          title: "Perfil atualizado",
+          description:
+            "Suas informações do perfil foram atualizados com sucesso",
         });
 
         setLoading(false);
@@ -76,12 +115,13 @@ const Profile: React.FC = () => {
 
         addToast({
           type: "error",
-          title: "Erro no cadastro",
-          description: "Ocorreu um erro ao fazer cadastro, cheque seus dados",
+          title: "Erro na atualização",
+          description:
+            "Ocorreu um erro ao atualizar seus dados, tente novamente",
         });
       }
     },
-    [addToast, history],
+    [addToast, history, updateUser],
   );
 
   const handleAvatarChange = useCallback(
