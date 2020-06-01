e617f437ed4386c14995b40f8b8b1fb8b7db5e89
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-31 22:33:31 -0300

    Ajusting rate limiter position

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -16,10 +16,10 @@ import "@shared/container";
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
