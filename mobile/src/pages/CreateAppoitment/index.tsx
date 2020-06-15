6f56ed9a3c7312314ef674bc9a302d85e9deb251
Matheus Pires <matheushenriquepires99@gmail.com>
2020-06-15 18:38:22 -0300

    Creating all pages and routes

---
+++ mobile/src/pages/CreateAppoitment/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const CreateAppointment: React.FC = () => {
+  return <View />;
+};
+
+export default CreateAppointment;
--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Dashboard from "../pages/Dashboard";
+import CreateAppoitment from "../pages/CreateAppoitment";
+import AppointmentCreated from "../pages/AppointmentCreated";
+import Profile from "../pages/Profile";
 
 const App = createStackNavigator();
 
@@ -13,6 +16,10 @@ const AppRoute: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppoitment" component={CreateAppoitment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
