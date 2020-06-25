264d5418f94398061e52da01ca20194e6ca32639
Matheus Pires <matheushenriquepires99@gmail.com>
2020-06-25 19:08:28 -0300

    Sign tests

--- frontend/src/__tests__/pages/SignIn.spec.tsx
+++ frontend/src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import SignIn from "../../pages/SignIn";
 
+const mockedHistoryPush = jest.fn();
+
 jest.mock("react-router-dom", () => {
   return {
-    useHistory: jest.fn(),
-    Link: ({ children }) => children,
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe("SignIn Page", () => {
   it("should be able to sign in", () => {
-    const { debug } = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText("E-mail");
+    const passwordField = getByPlaceholderText("Senha");
+    const buttonElement = getByText("Entrar");
+
+    fireEvent.change(emailField, { target: { value: "johndoe@example.com" } });
+    fireEvent.change(passwordField, { target: { value: "123123" } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith("/dashboard");
   });
 });
