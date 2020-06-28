8f6e5361c9fe6fe7724ab4f279a54be455b9465a
Matheus Pires <matheushenriquepires99@gmail.com>
2020-06-28 10:40:53 -0300

    Mocking authentication hook

--- frontend/src/__tests__/pages/SignIn.spec.tsx
+++ frontend/src/__tests__/pages/SignIn.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 
 import SignIn from "../../pages/SignIn";
 
@@ -14,8 +14,16 @@ jest.mock("react-router-dom", () => {
   };
 });
 
+jest.mock("../../hooks/auth", () => {
+  return {
+    useAuth: () => ({
+      signIn: jest.fn(),
+    }),
+  };
+});
+
 describe("SignIn Page", () => {
-  it("should be able to sign in", () => {
+  it("should be able to sign in", async () => {
     const { getByPlaceholderText, getByText } = render(<SignIn />);
 
     const emailField = getByPlaceholderText("E-mail");
@@ -27,6 +35,8 @@ describe("SignIn Page", () => {
 
     fireEvent.click(buttonElement);
 
-    expect(mockedHistoryPush).toHaveBeenCalledWith("/dashboard");
+    await wait(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith("/dashboard");
+    });
   });
 });
